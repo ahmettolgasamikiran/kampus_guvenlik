4b133a2255958e5f8066fd7a8835aecd83bbd6b8
Akif Ensar Doru <aedoru@172-2-1-13.lightspeed.dybhfl.sbcglobal.net>
2025-12-28 15:45:00 +0300

    Final: Tüm geliştirmeler birleştirildi ve yayına hazır

---
+++ lib/app_settings.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppSettings extends ChangeNotifier {
+  bool _isDarkMode = false;
+  bool _isEnglish = false;
+
+  bool get isDarkMode => _isDarkMode;
+  bool get isEnglish => _isEnglish;
+
+  // Tema değiştirme
+  void toggleTheme() {
+    _isDarkMode = !_isDarkMode;
+    notifyListeners();
+  }
+
+  // Dil değiştirme
+  void toggleLanguage() {
+    _isEnglish = !_isEnglish;
+    notifyListeners();
+  }
+}
